ed888ed68b0196c9ddf3a3ead42a4f8131649f91
peermutien <peermutien@gmail.com>
2024-02-12 18:05:44 +0530

    basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScreenComponent } from './screen/screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',  component:ScreenComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
